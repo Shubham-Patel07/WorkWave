143310373f015612e97ea90d62fa2e9b2b75c12c
Shubham-Patel07 <shubham.patel.btech2020@sitpune.edu.in>
2023-04-13 11:27:20 +0530

    Add: Vuestic Plugin installed

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
+import { createVuestic } from 'vuestic-ui'
+import 'vuestic-ui/css'
 
 const app = createApp(App)
 
 app.use(router)
 
+
+app.use(createVuestic());
 app.mount('#app')
